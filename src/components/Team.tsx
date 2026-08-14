69702d634b24f5f520974d2c5dcc1fa7bdf35fc8
Jorge Farias Campos <expatfyspain@gmail.com>
2026-08-14 10:34:27 +0200

    Fix Jorge surname

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -29,7 +29,7 @@ const team = [
     linkedin: "https://www.linkedin.com/in/vadyguerra/"
   },
   {
-    name: "Jorge Farías Campo",
+    name: "Jorge Farías Campos",
     role: "Ecosystem Builder",
     image: "https://cdn.prod.website-files.com/6596be80cb5d98bf1d75f148/6a7c3bae1107c4d28208c2c7_Perfil%20Jorge.jpeg",
     linkedin: "https://www.linkedin.com/in/jorgefariascampos/"
