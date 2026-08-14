58f9b59b69498edd02fbe81c9eaa6f7e4d64e7a9
Jorge Farias Campos <expatfyspain@gmail.com>
2026-08-14 10:40:58 +0200

    Match Jorge photo framing

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -85,9 +85,7 @@ const Team = () => {
                 <img
                   src={member.image}
                   alt={member.name}
-                  className={`w-full h-full object-cover group-hover:scale-108 transition-transform duration-500 ease-out ${
-                    member.name === "Jorge Farías Campo" ? "object-[center_20%]" : "object-center"
-                  }`}
+                  className="w-full h-full object-cover object-center group-hover:scale-108 transition-transform duration-500 ease-out"
                 />
                 
                 {/* Overlay on hover */}
